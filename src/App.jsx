b1fd2e72e25e2e5b5add2ab3bc70174d45fdd470
kayne776-netizen <kayne776@gmail.com>
2026-04-17 20:22:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -118,14 +118,13 @@ function App() {
     <div style={styles.app}>
       <aside style={styles.sidebar}>
         <div>
-          <div style={styles.logoBox}>
-            <div style={styles.logoCircle}>DCP</div>
-            <div>
-              <h2 style={styles.logoTitle}>DocControl Pro</h2>
-              <p style={styles.logoSub}>Project Intelligence Hub</p>
-            </div>
-          </div>
-
+         <div style={styles.logoBox}>
+  <div style={styles.logoCircle}>DCP</div>
+  <div>
+    <h2 style={styles.logoTitle}>DocControl Pro</h2>
+    <p style={styles.logoSub}>Project Intelligence Hub</p>
+  </div>
+</div>
           <div style={styles.sidebarMenu}>
             <button
               onClick={() => setActiveSection("Documents")}
